420a368919c0511c23668b0db02528fe77325ba1
OlegJakushkin <oleg.jakushkin@gmail.com>
2021-01-31 17:12:02 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j50
 
 cd ../../g2o
 
@@ -13,7 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j50
 
 cd ../../../
 
@@ -28,5 +28,5 @@ echo "Configuring and building ORB_SLAM2 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j4
+make -j50
 cd ..
